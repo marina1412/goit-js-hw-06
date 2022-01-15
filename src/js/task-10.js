d3c46106007f129a6b7b519ca2e4a47fe3769093
marina1412 <marinabalanchuk214@gmail.com>
2022-01-15 10:14:20 +0200

    task 1-9

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,31 @@
+const countOfDiv = document.getElementsByTagName('input');
+const number = countOfDiv.value;
+const buttonCreate = document.querySelector('button[data-create]');
+const buttonDestroy = document.querySelector('button[data-destroy]');
+const size = 20;
+const boxes = document.getElementById('boxes');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function createBoxes(amount) {
+  const array = [];
+  for (let i = 0; i <= amount; i += 1) {
+    const element = document.createElement('div');
+    element.style.width = size + 10*i + 'px';
+    element.style.height = size + 10*i + 'px';
+    element.style.color = red;
+    element.style.backgroundColor = getRandomHexColor;
+    array.push(element);
+    console.log(array);
+   }
+  return boxes.append(...array);
+}
+
+
+buttonCreate.addEventListener('click', createBoxes(number));
+ 
+
+
+
