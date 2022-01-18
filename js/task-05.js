35eca0842f000182e4d2f175e58bb5d9dccffa16
marina1412 <marinabalanchuk214@gmail.com>
2022-01-18 14:52:55 +0200

    task 5, 10 (correction 3)

--- js/task-05.js
+++ js/task-05.js
@@ -4,5 +4,5 @@ const defaultName = 'Anonymous';
 
 
 textInputRef.addEventListener('input', (event) => {
-    textOutputRef = event.currentTarget.value === '' ? textOutputRef.textContent = defaultName : textOutputRef.textContent = event.currentTarget.value;
+    textOutputRef.textContent = event.currentTarget.value === '' ? defaultName : event.currentTarget.value;
 })
--- js/task-10.js
+++ js/task-10.js
@@ -5,29 +5,29 @@ const inputValueRef = document.querySelector('input');
 const size = 20;
 
 const createBoxes = (amount) => {
+  const array = [];
   for (let i = 1; i <= amount; i += 1) { 
   const divEl = document.createElement('div');
   divEl.style.width = size + i*10 + 'px';
   divEl.style.height = size + i*10  + 'px';
   divEl.style.backgroundColor = getRandomHexColor();
-    boxesRef.appendChild(divEl);
-}
+  array.push(divEl);
+  }
+  inputValueRef.value = '';
+  boxesRef.append(...array);
 }
 
-const destroyBoxes = (amount) => {
-  for (let i = 1; i <= amount; i += 1) { 
-    const child = boxesRef.firstElementChild;
-    child.remove();
-    inputValueRef.value = '';
-    
-}
+const destroyBoxes = () => {
+  while (boxesRef.firstChild) {
+    boxesRef.removeChild(boxesRef.firstChild);
+  }
 }
 
 btnCreateRef.addEventListener('click', () => {
   createBoxes(inputValueRef.value);
 })
 btnDestroyRef.addEventListener('click', () => {
-  destroyBoxes(inputValueRef.value);
+  destroyBoxes();
 });
 
 function getRandomHexColor() {
